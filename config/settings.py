6ced418ffc18c2f100baed04bffec506137bec5e
Mikasa <kentorion1008@gmail.com>
2026-01-30 17:53:08 +0900

    fix host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(u%vswri0m0ud)@dlpmcsboqaw6!)@+l$4s@1hinai%(*ozf6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quantum-news-app.onrender.com']
 
 
 # Application definition
